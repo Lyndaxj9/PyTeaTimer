2e3d7f5714683966bf4f870f556a70e76523a4e6
Lynda <lyndaxj9@gmail.com>
2017-08-24 18:14:28 -0400

    Added comments to csv_reader

--- csv_reader.py
+++ csv_reader.py
@@ -1,8 +1,12 @@
 #!/usr/local/bin/python3
 import csv
+""" Script that takes the info from a .csv file with a header 
+and returns a list of tuples of the data from the .csv file
+"""
 
 
 def read_file(file_name):
+    """ Reads data from .csv file skips the header and returns a list with all the data """
     alldata = []
     with open(file_name, newline='') as csvfile:
         filtered = (line.replace('\r', '\n') for line in csvfile)  # get rid of carriage return
@@ -14,11 +18,16 @@ def read_file(file_name):
 
 
 def print_data(list_data):
+    """ Prints data from list with each tuple in a new line"""
     for r in list_data:
         print(r)
 
 
 if __name__ == '__main__':
+    defaultfile = "teainfo.csv"
     filename = input("What is the file name?: ")
+
+    if filename == "":
+        filename = defaultfile
     newlist = read_file(filename)
-    print_data(newlist[12])
+    print_data(newlist)
