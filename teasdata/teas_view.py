b9249bbd7d008124d9ec27d11e524f481b4a3b41
Lynda <lyndaxj9@gmail.com>
2017-09-09 21:13:03 -0400

    Have basic view for a single tea set up

--- teasdata/teas_view.py
+++ teasdata/teas_view.py
@@ -10,12 +10,16 @@ class TeasView:
         self.__headerformat = "|{0:^3s}|{1:^25s}|{2:^8s}|{3:^8s}|{4:^8s}|"
         self.__teasformat = "|{0:^3d}|{1:^25s}|{2:^8s}|{3:^8d}|{4:^8s}|"
         self.__headers = self.__headerformat.format('#', 'Name', 'Type', 'Temp'+self.__degreesign+'F', 'Time')
-        self.__seperator = "=" * 58
+        self.__seperator00 = "=" * 58
+        self.__seperator01 = "-" * 58
 
         self.__namebrandformat = "|{0:24s} ({2:^8s})|Brand: {1:13s}|"
         self.__temppkgformat = "|{0:<35s}|Packaging: {1:9s}|"
         self.__timepriceformat = "|{0:02d}:{1:02d} {3:29s}|Price: ${2:3.2f}{4:>9}"
         self.__timepriceformat01 = "|{0:02d}:{1:02d} {3:29s}|Price: ${2:^12s}|"
+        self.__notesformat00 = "|Notes: {0:49s}|"
+        self.__notesformat01 = "|{0:56s}|"
+        self.__buyhandformat = "|Buy Again: {0:24s}|On Hand: {1:11s}|"
 
         self.__prompt00 = "'S' to (S)elect a tea | 'B' to go (B)ack"
         self.__prompt01 = "'A' to view (A)ll teas"
@@ -39,7 +43,13 @@ class TeasView:
         """ Prints one of the errors selected by the in_errornum param. """
         print(self.__errors[in_errornum])
 
-    # TODO better format price print
+    def notes_printer(self, tea_notes):
+        notelines = tea_notes.split('\n')
+        print(self.__notesformat00.format(notelines[0]))
+        if len(notelines) > 1:
+            for i in range(1, len(notelines)):
+                print(self.__notesformat01.format(notelines[i]))
+
     def one_tea_display(self, in_tea):
         """ Print information about one tea that is passed in. """
         in_tea = in_tea[0]
@@ -53,8 +63,10 @@ class TeasView:
             print(self.__timepriceformat.format(int(timeparts[0]), int(timeparts[1]), in_tea[10], "mins", "|"))
         else:
             print(self.__timepriceformat01.format(int(timeparts[0]), int(timeparts[1]), "-", "mins"))
-        print(in_tea)
-        print(self.__seperator)
+        print(self.__seperator01)
+        self.notes_printer(in_tea[5])
+        print(self.__buyhandformat.format(in_tea[8], in_tea[9]))
+        print(self.__seperator00)
         print(self.__prompt01)
 
     def all_teas_display(self, all_teas):
@@ -63,6 +75,6 @@ class TeasView:
         if all_teas is not None:
             for i, row in enumerate(all_teas):
                 print(self.__teasformat.format(i+1, row[1], row[2], row[3], row[4]))
-            print(self.__seperator)
+            print(self.__seperator00)
         else:
             print(self.__error02)
